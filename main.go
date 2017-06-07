b0dfa1df7757c80cb9f0f8dba37eb3ca49598219
Andrea Funtò <dihedron.dev@gmail.com>
2017-06-07 15:56:57 +0200

    Started implementing editor.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"os"
 
 	"github.com/hashicorp/terraform/plugin"
@@ -8,7 +9,29 @@ import (
 
 func main() {
 	if len(os.Args) > 1 {
-		// TODO: editor mode
+		// editor mode
+		file, _ := os.Open(os.Args[1])
+		d := xml.NewDecoder(file)
+		for {
+			t, tokenErr := d.Token()
+			if tokenErr != nil {
+				// TODO: log error
+			}
+			switch t := t.(type) {
+			case xml.StartElement:
+				if t.Name.Space == "foo" && t.Name.Local == "bar" {
+					/*
+						var b bar
+						if err := d.DecodeElement(&b, &t); err != nil {
+							// handle error
+						}
+					*/
+					// do something with b
+				}
+				// TODO: add other cases
+			}
+		}
+
 	} else {
 		// terraform plugin mode
 		plugin.Serve(&plugin.ServeOpts{
